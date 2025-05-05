afcd17cbb842b79c0e6d49b433b32bfdb0e410dc
Laure Thompson <602628+laurejt@users.noreply.github.com>
2025-05-05 11:22:08 -0400

    Updates based on @rlskoeser feedback

--- notebooks/find-quotes.py
+++ notebooks/find-quotes.py
@@ -18,7 +18,8 @@ def _():
 def _(mo):
     mo.md(
         """
-        # Finding general quotes in Die Neue Zeit
+        # Finding General Quotes in Die Neue Zeit
+
         This notebook explores finding quotes within Die Neue Zeit article using simple search rules.
         Namely, that a quote is text that occurs between the starting quote character „ and an ending quote character (either “ or ").
         In practice, this identifies quotations as well as titles and concepts.
@@ -82,7 +83,32 @@ def _(mo):
 
 
 @app.cell
+def _():
+    # A series of page-level notes indexed by page id, written by Laure Thompson
+    notes = {
+        18: """- Corresponds to page 25""",
+        38: """- Corresponds to page 43\n- Missing leading quote mark: "Frage an:\n<mark>„</mark>Bet gewissem"\n- Spurious quote mark: "die Naturprodukte nicht!<mark>"</mark> Hier"
+        """,
+        76: """- Corresponds to page 76\n- Missing ending quote mark, transcribed as period: "und Schön¬
+    geister<mark>.</mark>\n— heißt es\n- Missing ending quote mark: "die Verwirklichung der Philssophie<mark>“</mark> (a. a. O. S. 85)."
+    """,
+        256: """- Corresponds to page 231""",
+        301: """- Corresponds to page 268\n- Ending block quote not captured because it continues to the next page: "„Die Kommunisten [...]"
+        """,
+        499: """- Corresponds to page 289""",
+        545: """- Corresponds to page 493\n- Missing ending quote, transcribed as ¬: "die „Heilige Familie<mark>¬</mark> zurück"
+        """,
+        546: """- Corresponds to page 494\n- Spurious quote mark: "Produktionsprozesse\nleitet,<mark>“</mark> wiedergefunden"
+        """,
+        549: """- Corresponds to page 497\n- Potentially incorrect annotations. Does this page contain any direct quotations from Kapital, or are there just citations (e.g., the footnote)? - The text seems to be missing the closing quotation for a referenced title. As a result, several title mentions are grouped together.""",
+        661: """- Corresponds to page 612""",
+    }
+    return (notes,)
+
+
+@app.cell
 def _(mo, page_data):
+    # Create slider for browsing page-level results
     page_slider = mo.ui.slider(start=1, stop=len(page_data.rows()), value=2)
     return (page_slider,)
 
@@ -99,31 +125,7 @@ def _(mo, page_slider):
     return
 
 
-@app.cell
-def _():
-    # Store notes for pages (index by page id)
-    notes = {
-        18: """- Corresponds to page 25""",
-        38: """- Corresponds to page 43\n- Missing leading quote mark: "Frage an:\n<mark>**„**</mark>Bet gewissem"\n- Spurious quote mark: "die Naturprodukte nicht!<mark>**"**</mark> Hier"
-        """,
-        76: """- Corresponds to page 76\n- Missing ending quote mark, transcribed as period: "und Schön¬
-    geister<mark>**.**</mark>\n— heißt es\n- Missing ending quote mark: "die Verwirklichung der Philssophie<mark>**“**</mark> (a. a. O. S. 85)."
-    """,
-        256: """- Corresponds to page 231""",
-        301: """- Corresponds to page 268\n- Ending block quote not captured because it continues to the next page: "„Die Kommunisten [...]"
-        """,
-        499: """- Corresponds to page 289""",
-        545: """- Corresponds to page 493\n- Missing ending quote, transcribed as ¬: "die „Heilige Familie<mark>**¬**</mark> zurück"
-        """,
-        546: """- Corresponds to page 494\n- Spurious quote mark: "Produktionsprozesse\nleitet,<mark>**“**</mark> wiedergefunden"
-        """,
-        549: """- Corresponds to page 497\n- Potentially incorrect annotations. Does this page contain any direct quotations from Kapital, or are there just citations (e.g., the footnote)? - The text seems to be missing the closing quotation for a referenced title. As a result, several title mentions are grouped together.""",
-        661: """- Corresponds to page 612""",
-    }
-    return (notes,)
-
-
-@app.cell
+@app.cell(hide_code=True)
 def _(find_quotes, mo, notes, page_data, page_slider):
     row_id = page_slider.value - 1
     page_id = page_data.row(row_id)[1]
@@ -139,14 +141,14 @@ def _(find_quotes, mo, notes, page_data, page_slider):
         start, end = match.span()
         highlighted_page = (
             highlighted_page[:start]
-            + "<mark>**"
+            + "<mark>"
             + highlighted_page[start:end]
-            + "</mark>**"
+            + "</mark>"
             + highlighted_page[end:]
         )
     results.append(mo.md(f"### Page with Quotes Highlighted\n{highlighted_page}"))
     # Print raw page
-    print("Raw Page Text:\n" + page_text)
+    print("Raw Page Text:\n\n" + page_text)
     mo.vstack(results)
     return
 
