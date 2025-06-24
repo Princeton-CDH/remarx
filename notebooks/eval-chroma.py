59795c0ab753ae53b97fd0165f1c430e0ea76cf9
Laure Thompson <602628+laurejt@users.noreply.github.com>
2025-06-24 14:00:02 -0400

    Adds eval utilities & chroma eval notebook

---
+++ notebooks/eval-chroma.py
@@ -0,0 +1,203 @@
+import marimo
+
+__generated_with = "0.13.11"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import polars as pl
+    import seaborn as sns
+
+    from remarx.eval_knn import (
+        precision_at_distance,
+        recall_at_distance,
+        recall_at_k,
+    )
+    return (
+        mo,
+        np,
+        pl,
+        plt,
+        precision_at_distance,
+        recall_at_distance,
+        recall_at_k,
+        sns,
+    )
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""# Evaluating ChromaDB Results""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Load Data""")
+    return
+
+
+@app.cell
+def _(pl):
+    # Load in the sentence pair results for ChromaDB for 10-nearest neighbors
+    all_pairs_df = pl.read_csv("data/sentence-pairs/chromadb-top10.csv")
+    all_pairs_df
+    return (all_pairs_df,)
+
+
+@app.cell
+def _(all_pairs_df, pl):
+    # Filter to known matching pairs (i.e., true positives)
+    true_pairs_df = all_pairs_df.filter(pl.col("is_match"))
+    true_pairs_df
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    ## Recall@k
+    First, we'll see how many nearest neighbors it takes to find our test set of DNZ-Marx sentence pairs.
+    """
+    )
+    return
+
+
+@app.cell
+def _(all_pairs_df, pl, plt, recall_at_k, sns):
+    max_rank = all_pairs_df.get_column("rank").max()
+    rank_recall_results = []
+    for i in range(1, max_rank + 2):
+        rank_recall_results.append(
+            {"rank": i, "rank@k": recall_at_k(all_pairs_df, i)}
+        )
+
+    sns.pointplot(data=pl.DataFrame(rank_recall_results), x="rank", y="rank@k")
+    plt.xlabel("Rank")
+    plt.ylabel("Recall")
+    plt.title("Recall@k")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        rf"""As we can see, the majority of matching DNZ-Marx sentence pairs are the first nearest neighbor. However, we need the top five nearest neighbors in order to retrieve all matching pairs."""
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    ## Examining Sentence Distances
+    We'll try a few visualizations to get a sense of the distance values for all retrieved DNZ-Marx sentences pairs, both correct and incorrect matches.
+    """
+    )
+    return
+
+
+@app.cell
+def _(all_pairs_df, pl):
+    # For drawing figures
+    true_dists = (
+        all_pairs_df.filter(pl.col("is_match")).get_column("distance").to_list()
+    )
+    return (true_dists,)
+
+
+@app.cell
+def _(all_pairs_df, plt, sns, true_dists):
+    ax_dists = sns.histplot(all_pairs_df, x="distance", stat="count")
+    # Add vertical lines to mark matches
+    for dist2 in true_dists:
+        ax_dists.axvline(x=dist2, color="orange", linestyle="--")
+    plt.title("DNZ-Marx Distances\n(orange lines indicate true matches)")
+    plt.show()
+    return
+
+
+@app.cell
+def _(all_pairs_df, np, plt, sns, true_dists):
+    plt.figure(figsize=(10, 3))
+    ax_strip = sns.stripplot(
+        all_pairs_df,
+        x="distance",
+        hue="is_match",
+        jitter=0.25,
+        alpha=0.6,
+        legend=False,
+    )
+    ax_strip.set_xticks(np.arange(0, 1.9, 0.1))
+    for dist1 in true_dists:
+        ax_strip.axvline(x=dist1, color="orange", linestyle="--")
+    ax_strip.set_title("DNZ-Marx Distances\n(true matches in orange)")
+    plt.show()
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""In good news, the smallest distances correspond exclusively to matching sentence pairs. However, two of the matching pairs have distances higher than many incorrect sentence pairs."""
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Evaluating Distance Thresholds""")
+    return
+
+
+@app.cell
+def _(
+    all_pairs_df,
+    np,
+    pl,
+    plt,
+    precision_at_distance,
+    recall_at_distance,
+    sns,
+    true_dists,
+):
+    plt.figure(figsize=(10, 3))
+    dist_results = []
+
+    for d in np.arange(0.1, 0.95, 0.05):
+        dist_results.append(
+            {
+                "distance": d,
+                "score": precision_at_distance(all_pairs_df, d),
+                "measure": "precision",
+            }
+        )
+        dist_results.append(
+            {
+                "distance": d,
+                "score": recall_at_distance(all_pairs_df, d),
+                "measure": "recall",
+            }
+        )
+
+    ax_points = sns.pointplot(
+        pl.DataFrame(dist_results),
+        x="distance",
+        y="score",
+        hue="measure",
+        native_scale=True,
+    )
+    ax_points.axvline(x=max(true_dists), linestyle=":", color="black")
+    plt.text(max(true_dists) + 0.01, 0.5, "max dist for\n   matches")
+    plt.title("Precision and Recall for Distance Thresholds")
+    return
+
+
+if __name__ == "__main__":
+    app.run()
