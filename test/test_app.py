154c95566a4361b4383394e37b0d202775874f74
Laure Thompson <602628+laurejt@users.noreply.github.com>
2025-08-08 08:26:50 -0400

    Completed unit tests, fixed dev group

--- test/test_app.py
+++ test/test_app.py
@@ -1,6 +1,20 @@
-from remarx import app
+from unittest.mock import patch
+
+import remarx
+from remarx.app import app
 
 
-def test_app():
-    # Check this method terminates
+@patch("remarx.app.marimo.running_in_notebook", return_value=True)
+def test_app_as_nb(mock_run_check):
+    # Simulate running as notebook
     app.run()
+    mock_run_check.assert_called_once()
+
+
+@patch("remarx.app.marimo.running_in_notebook", return_value=False)
+@patch("remarx.app.marimo._cli.cli")
+def test_app_as_script(mock_cli, mock_run_check):
+    # Simulate running as script
+    app.run()
+    mock_run_check.assert_called_once()
+    mock_cli.main.assert_called_once_with(["run", remarx.app.__file__])
