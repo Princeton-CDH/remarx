3b3314954d5e1a26760f292419dc38e080e96cdc
Laure Thompson <602628+laurejt@users.noreply.github.com>
2025-08-08 13:30:51 -0400

    First draft of app

--- src/remarx/app.py
+++ src/remarx/app.py
@@ -6,13 +6,12 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
-    import subprocess
-    import sys
-
+    import csv
     import marimo as mo
 
     import remarx
-    return mo, remarx, sys
+    from remarx.sentence.segment import segment_text
+    return csv, mo, remarx
 
 
 @app.cell
@@ -42,11 +41,17 @@ def _(mo):
 
 
 @app.cell
+def _(mo, remarx):
+    mo.md(rf"""Running `remarx` version: {remarx.__version__}""")
+    return
+
+
+@app.cell
 def _(mo):
     mo.md(
         r"""
-    ## Check `remarx` version number
-    Click the button below to display the running version number of `remarx`.
+    ## Sentence Corpus Builder
+    In this part of the application, a sentence corpus (`CSV`) will be created for a selected text file.
     """
     )
     return
@@ -54,28 +59,95 @@ def _(mo):
 
 @app.cell
 def _(mo):
-    version_button = mo.ui.run_button(label="Show Version")
-    return (version_button,)
-
-
-@app.cell
-def _(version_button):
-    version_button
+    mo.md(
+        r"""
+    ### Select Input Text File
+    Please select the text file whose sentences will be extracted to build a sentence corpus.
+    """
+    )
     return
 
 
 @app.cell
-def _(mo, remarx, version_button):
-    display_text = None
-    callout_type = None
-    if version_button.value:
-        display_text = f"**remarx version:** {remarx.__version__}"
-        callout_type = "neutral"
-    else:
-        display_text = "*Click the button above!*"
-        callout_type = "info"
+def _(mo):
+    select_input_file = mo.ui.file(kind="area", filetypes=[".txt"])
+    select_input_file
+    return (select_input_file,)
 
-    mo.md(display_text).callout(callout_type)
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    ## Select Save Location
+    Select the folder where the resulting corpus will be saved.
+    The name of the csv will be the same as the input file's.
+    """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    select_output_dir = mo.ui.file_browser(
+        selection_mode="directory",
+        multiple=False,
+    )
+    select_output_dir
+    return (select_output_dir,)
+
+
+@app.cell
+def _(select_input_file, select_output_dir):
+    is_ready = any(select_input_file.value) and any(select_output_dir.value)
+    is_ready
+    return (is_ready,)
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    ### Build Sentence Corpus
+    Click the "Build Corpus" to run `remarx`.
+    The sentence corpus for the input text will be saved as a CSV in the selected save location.
+    This output file will have the same filename (but different file extension) as the selected input file.
+
+    The button will be disabled (grayed out) until both the input text file and save location have been selected.
+    """
+    )
+    return
+
+
+@app.cell
+def _(is_ready, mo):
+    button = mo.ui.run_button(disabled=not is_ready, label="Build Corpus")
+    button
+    return (button,)
+
+
+@app.cell
+def _(button, csv, remarx, select_input_file, select_output_dir):
+    # Build Sentence Corpus
+    if button.value:
+        # Get input values
+        input_file = select_input_file.value[0]
+        output_dir = select_output_dir.value[0]
+        # Determine output csv path
+        output_csv = (output_dir.path / input_file.name).with_suffix(".csv")
+
+        print(f"Building Sentence Corpus for {input_file.name}...")
+        file_text = input_file.contents.decode("utf-8")
+        sentences = remarx.sentence.segment.segment_text(file_text)
+
+        print(f"Saving corpus {output_csv.name} to {output_dir.path}")
+        with open(output_csv, mode="w", newline="") as file_handler:
+            fieldnames = ["id", "offset", "text"]
+            writer = csv.writer(file_handler)
+            writer.writerow(fieldnames)
+            for sentence in sentences:
+                writer.writerow(sentence)
+        print("Done!")
     return
 
 
