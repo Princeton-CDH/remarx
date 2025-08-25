8aeea2643d65879692a6767d53a339340c54d82e
hao <97079365+tanhaow@users.noreply.github.com>
2025-08-25 13:30:18 -0400

    Update __init__.py

--- src/remarx/sentence/__init__.py
+++ src/remarx/sentence/__init__.py
@@ -3,6 +3,5 @@ This module contains libraries for sentence segmentation and sentence corpus con
 """
 
 from remarx.sentence import corpus, segment
-from remarx.sentence.segment import segment_text
 
 __all__ = ["corpus", "segment", "segment_text"]
