96eb2e0ce451ed64f53b94c30c3c0eac08a7d17b
hao <97079365+tanhaow@users.noreply.github.com>
2025-08-25 11:57:58 -0400

    add imports to inits

--- src/remarx/sentence/__init__.py
+++ src/remarx/sentence/__init__.py
@@ -1,3 +1,8 @@
 """
 This module contains libraries for sentence segmentation and sentence corpus construction.
 """
+
+from remarx.sentence import corpus, segment
+from remarx.sentence.segment import segment_text
+
+__all__ = ["corpus", "segment", "segment_text"]
