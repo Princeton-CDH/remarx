ecc367cf9b8475fcfbec6cbdcb77809ba7df2f15
Hao Tan <ht8933@princeton.edu>
2025-09-18 12:39:46 -0400

    Update src/remarx/quotation/__init__.py
    Co-authored-by: Laure Thompson <602628+laurejt@users.noreply.github.com>

--- src/remarx/quotation/__init__.py
+++ src/remarx/quotation/__init__.py
@@ -1,5 +1,5 @@
 """
-This module contains functionality for embedding generation and quotation detection.
+This module contains libraries for embedding generation and quotation detection.
 """
 
 from remarx.quotation import embeddings
