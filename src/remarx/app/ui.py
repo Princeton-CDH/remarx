b81614a21cd68a36eef9a71175e3d45a1515704f
Laure Thompson <602628+laurejt@users.noreply.github.com>
2025-09-18 09:22:29 -0400

    Fix app docs

--- src/remarx/app/ui.py
+++ src/remarx/app/ui.py
@@ -1,6 +1,7 @@
 """
-The marimo notebook corresponding to the `remarx` application. The application
-can be launched by running the command `remarx-app` or via marimo.
+The marimo notebook corresponding to the `remarx` app.
+
+The app can be launched by running the `remarx-app` command or via marimo.
 
 Example Usage:
 
