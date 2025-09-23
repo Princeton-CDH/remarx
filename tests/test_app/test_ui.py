3b7e808dfe8789a631dddd1b2eeda8aff7b9d09c
Laure Thompson <602628+laurejt@users.noreply.github.com>
2025-09-23 14:18:23 -0400

    Add unit test for app logging

--- tests/test_app/test_ui.py
+++ tests/test_app/test_ui.py
@@ -4,3 +4,17 @@ from remarx.app.ui import app
 def test_app():
     # Check application does not crash when run programmatically
     app.run()
+
+
+def test_app_logging(tmp_path, monkeypatch):
+    # Check default logging for app
+    monkeypatch.chdir(tmp_path)
+    app.run()
+
+    log_files = list((tmp_path / "logs").iterdir())
+    assert len(log_files) == 1
+    log_text = log_files[0].read_text()
+    # Has one log
+    assert log_text.count("\n") == 1
+    # Validate log
+    assert log_text.endswith("INFO:remarx-app::Remarx UI notebook started\n")
