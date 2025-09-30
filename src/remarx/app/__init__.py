bfe090c489d82aa3a147d2c69d586db7f343dff7
Hao Tan <ht8933@princeton.edu>
2025-09-30 17:20:04 -0400

    Feature/refactor app (#228)
    * Removed accordion
    
    * Preliminary multi-notebook app (still a WIP)
    
    * Revise per @rlskoeser review
    
    * Update CHANGELOG.md
    
    * Add more tests to pass CodeCov
    
    * add dependency for the new unit tests
    
    * create a separate function that returns just the redirect route
    
    * Add logging and use `run` for testing notebook app
    
    * Configure logging once at startup to prevent multiple log files
    
    * add comments
    
    * add tests for logging
    
    * Added my suggested changes
    
    ---------
    
    Co-authored-by: Laure Thompson <602628+laurejt@users.noreply.github.com>

--- src/remarx/app/__init__.py
+++ src/remarx/app/__init__.py
@@ -2,6 +2,6 @@
 This module contains the UI and utilities for the remarx app
 """
 
-from remarx.app import ui, utils
+from remarx.app import corpus_builder, quote_finder, utils
 
-__all__ = ["ui", "utils"]
+__all__ = ["corpus_builder", "quote_finder", "utils"]
