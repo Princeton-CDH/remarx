26acf5b643226f1401dbda1fc06bb09f733b2547
hao <97079365+tanhaow@users.noreply.github.com>
2025-09-30 12:39:17 -0400

    add comments

--- tests/test_app/test_ui.py
+++ tests/test_app/test_ui.py
@@ -2,10 +2,10 @@ from remarx.app import corpus_builder, quote_finder
 
 
 def test_corpus_builder_app():
-    # Run the app - if it crashes, the test will fail
+    # Test by running the notebook app - if it crashes, the test will fail
     corpus_builder.app.run()
 
 
 def test_quote_finder_app():
-    # Run the app - if it crashes, the test will fail
+    # Test by running the notebook app - if it crashes, the test will fail
     quote_finder.app.run()
