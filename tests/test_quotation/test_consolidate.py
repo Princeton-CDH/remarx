646510b6c20f8453f30499486284d4f6595be658
rlskoeser <rebecca.s.koeser@princeton.edu>
2025-12-02 13:46:28 -0500

    Revise consolidation logic to only group sequences within files

--- tests/test_quotation/test_consolidate.py
+++ tests/test_quotation/test_consolidate.py
@@ -12,15 +12,16 @@ def test_identify_sequences():
                 1,
                 2,
                 3,
-            ]
+            ],
+            "group": ["a", "a", "a"],
         }
     )
-    df_seq = identify_sequences(df, "idx")
+    df_seq = identify_sequences(df, "idx", "group")
     # adds a group field based on the specified field
     assert "idx_group" in df_seq.columns
     assert "idx_sequential" in df_seq.columns
-    # does not add any other columns
-    # assert len(df_seq.columns) == 3  # original idx + two new
+    # only adds the expected additional columns
+    assert len(df_seq.columns) == len(df.columns) + 2  #  two new columns
 
     # in this case, all rows are in a single sequence, starting with idx 1
     assert all(df_seq["idx_group"].eq(1))
@@ -28,24 +29,38 @@ def test_identify_sequences():
     assert all(df_seq["idx_sequential"].eq(True))
 
     # test with a subset that is sequential
-    df = pl.DataFrame(data={"idx": [1, 3, 4, 5, 7]})
-    df_seq = identify_sequences(df, "idx")
+    df = pl.DataFrame(data={"idx": [1, 3, 4, 5, 7], "group": ["a", "a", "a", "a", "a"]})
+    df_seq = identify_sequences(df, "idx", "group")
     assert df_seq["idx_group"].to_list() == [1, 3, 3, 3, 7]
     assert df_seq["idx_sequential"].to_list() == [False, True, True, True, False]
 
     # test with non sequential
-    df = pl.DataFrame(data={"idx": [2, 4, 6, 8]})
-    df_seq = identify_sequences(df, "idx")
+    df = pl.DataFrame(data={"idx": [2, 4, 6, 8], "group": ["a", "a", "a", "a"]})
+    df_seq = identify_sequences(df, "idx", "group")
     assert df_seq["idx_group"].to_list() == [2, 4, 6, 8]
     # none are sequential
     assert all(df_seq["idx_sequential"].eq(False))
+
+    # test sequential but different groups
+    df = pl.DataFrame(data={"idx": [1, 2, 3], "group": ["a", "b", "c"]})
+    df_seq = identify_sequences(df, "idx", "group")
+    assert df_seq["idx_group"].to_list() == [1, 2, 3]
+    # none are sequential
+    assert all(df_seq["idx_sequential"].eq(False))
+
+    # sequential with groups - one grouped sequence
+    df = pl.DataFrame(data={"idx": [1, 2, 3], "group": ["a", "a", "b"]})
+    df_seq = identify_sequences(df, "idx", "group")
+    assert df_seq["idx_group"].to_list() == [1, 1, 3]
+    # first two are part of a sequence
+    assert df_seq["idx_sequential"].to_list() == [True, True, False]
 
 
 def test_consolidate_quotes():
     #  simple case: two rows, sequential on both sides
     reuse_text = ["A sentence.", "Another."]
     orig_text = ["A short sentence.", "Another sentence."]
-    orig_file = ["one.txt", "two.txt"]
+    orig_file = "one.txt"
     reuse_file = "abc.txt"
     df = pl.DataFrame(
         data={
@@ -57,7 +72,7 @@ def test_consolidate_quotes():
             "original_id": ["o1", "o2"],
             "original_sent_index": [5, 6],
             "original_text": orig_text,
-            "original_file": orig_file,  # different
+            "original_file": [orig_file, orig_file],  # same
         }
     )
 
@@ -74,10 +89,18 @@ def test_consolidate_quotes():
     assert result["original_text"] == " ".join(orig_text)
     # other fields are combined with semi-colon (unique values only)
     assert result["reuse_file"] == reuse_file  # both rows have the same
-    assert result["original_file"] == "; ".join(orig_file)
+    assert result["original_file"] == orig_file
 
     # confirm columns are as expected (no extra beyond num_sentences)
+    assert "num_sentences" in df_consolidated.columns
     assert len(df_consolidated.columns) == len(df.columns) + 1
+
+    # test that sequential sentences in _different_ files are not consolidated
+    # same test df, but update so original sentences are from different files
+    orig_multifile = ["one.txt", "two.txt"]
+    df_multifile = df.update(pl.DataFrame(data={"original_file": orig_multifile}))
+    df_consolidated = consolidate_quotes(df_multifile)
+    assert len(df_consolidated) == 2
 
     # test that non-sequential rows are returned as-is
     nonseq_df = pl.DataFrame(
@@ -103,9 +126,20 @@ def test_consolidate_quotes():
     assert len(mixed_df_consolidated) == 2
 
     # no data but required fields - should not error
-    empty_df = pl.DataFrame(data={"reuse_sent_index": [], "original_sent_index": []})
-    assert isinstance(consolidate_quotes(empty_df), pl.DataFrame)
-
-    empty_df = pl.DataFrame()
-    with pytest.raises(pl.exceptions.ColumnNotFoundError):
+    empty_df = pl.DataFrame(
+        data={
+            "reuse_sent_index": [],
+            "original_sent_index": [],
+            "reuse_file": [],
+            "original_file": [],
+        }
+    )
+    with pytest.raises(ValueError, match="Cannot consolidate"):
         consolidate_quotes(empty_df)
+
+    with pytest.raises(ValueError, match="Cannot consolidate"):
+        consolidate_quotes(pl.DataFrame())
+
+    wrong_df = pl.DataFrame(data={"foo": ["bar", "baz"]})
+    with pytest.raises(pl.exceptions.ColumnNotFoundError):
+        consolidate_quotes(wrong_df)
